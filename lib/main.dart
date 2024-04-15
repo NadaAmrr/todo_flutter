b37c0c12d61a6ad3ab0114597ca1bb073a31cbcb
Nada <nada.amr1267@gmail.com>
2024-04-16 01:16:13 +0200

    Main

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/app/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseFirestore.instance.disableNetwork();
+  runApp(const MyApp());
+}
