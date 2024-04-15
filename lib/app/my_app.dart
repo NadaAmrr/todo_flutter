0fcc53b942b04299a1fd5e336a74289f744f12f4
Nada <nada.amr1267@gmail.com>
2024-04-16 01:14:06 +0200

    My app

---
+++ lib/app/my_app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo_app/core/utils/app_strings.dart';
+import 'package:todo_app/core/utils/app_theme.dart';
+import 'package:todo_app/features/edit/presentation/pages/edit_page.dart';
+import 'package:todo_app/features/home/presentation/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(412, 870),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: AppStrings.appName,
+        theme: AppTheme.darkTheme,
+        darkTheme: AppTheme.darkTheme,
+        initialRoute: HomePage.routeName,
+        routes: {
+          HomePage.routeName: (context) => const HomePage(),
+          EditPage.routeName: (context) => EditPage(),
+        },
+      ),
+    );
+  }
+}
